64334e7afa32c9c232868afe1747b902c944e894
Benjamin Abel <dev.abel@free.fr>
2023-07-31 21:48:46 +0200

    nevbar

--- _assets/global.js
+++ _assets/global.js
@@ -15,17 +15,6 @@ function renderDemos() {
       );
     }
   }
-
-  if (!document.documentElement.matches(".no-home-link")) {
-    let h1 = document.querySelector("h1");
-
-    if (h1 && !h1.parentNode.querySelector(".home")) {
-      h1.insertAdjacentHTML(
-        "beforebegin",
-        `<a href="../index.html" class="home">Nude UI</a>`
-      );
-    }
-  }
 }
 
 renderDemos();
